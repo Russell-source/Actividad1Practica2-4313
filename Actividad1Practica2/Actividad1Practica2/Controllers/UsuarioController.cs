342ac1f577ea0f1ec70a649881d1dbf8e0933d44
Russell-source <fp161542@gmail.com>
2025-09-23 19:10:13 +0200

    Actualizar

---
+++ Actividad1Practica2/Actividad1Practica2/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TuProyecto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Datos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Datos(string nombre, DateTime fechaNacimiento)
+        {
+            int edad = CalcularEdad(fechaNacimiento);
+            int añoNacimiento = fechaNacimiento.Year;
+
+            ViewBag.Mensaje = $"Hola, {nombre}. Tienes {edad} años y tu año de nacimiento es {añoNacimiento}.";
+            return View();
+        }
+
+        private int CalcularEdad(DateTime fechaNacimiento)
+        {
+            var hoy = DateTime.Today;
+            int edad = hoy.Year - fechaNacimiento.Year;
+            if (fechaNacimiento.Date > hoy.AddYears(-edad)) edad--;
+            return edad;
+        }
+    }
+}
